5cb38db0f8a8a8f240c8adc5ee878dec697e1dd7
rem official <126304667+shanachanpanel@users.noreply.github.com>
2025-03-25 11:54:18 +0700

    Update getOngoingAnime.ts

--- lib/getOngoingAnime.ts
+++ lib/getOngoingAnime.ts
@@ -1,7 +1,7 @@
 import { getBaseUrl } from "./getBaseUrl";
 
 export const getOngoingAnime = async (page: string | number = 1) => {
-  const populars = await fetch(`${getBaseUrl()}/kuramanime/ongoing/popular?page=${page}`, {
+  const populars = await fetch(`${getBaseUrl()}/otakudesu/ongoing?page=${page}`, {
     headers: { "content-type": "aplication/json" },
   });
   const json = populars.json();
