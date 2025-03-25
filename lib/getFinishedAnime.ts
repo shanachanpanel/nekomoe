438ba629914f121838ecf66b67bb8249665663e1
rem official <126304667+shanachanpanel@users.noreply.github.com>
2025-03-25 11:56:04 +0700

    Update getFinishedAnime.ts

--- lib/getFinishedAnime.ts
+++ lib/getFinishedAnime.ts
@@ -1,7 +1,7 @@
 import { getBaseUrl } from "./getBaseUrl";
 
 export const getFinishedAnime = async (page: string | number = 1) => {
-  const populars = await fetch(`${getBaseUrl()}/kuramanime/ongoing/popular?page=${page}`, {
+  const populars = await fetch(`${getBaseUrl()}/samehadaku/ongoing?page=${page}`, {
     headers: { "content-type": "aplication/json" },
   });
   const json = populars.json();
